24cee5cb06732a82fd593fe22728cda2d1ee74a1
mahotilo <kvmahotilo@gmail.com>
2020-03-10 01:05:02 +0200

    add missed IP detection

--- LocalServerAlert.php
+++ LocalServerAlert.php
@@ -15,9 +15,10 @@ class LocalServerAlert {
   public static function PageRunScript($cmd) {
     global $page, $dirPrefix;
     if( \gp\tool::LoggedIn() ){
-		if ( !filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) )
+    	$IP = $_SERVER['SERVER_ADDR'];
+		if ( !filter_var($IP, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) )
 		{
-			array_unshift($page->admin_links, '<i class="fa fa-lock" style="font-size: x-large; vertical-align: bottom;" title="The site is run on a local server" data-toggle="tooltip"></i>');
+			array_unshift($page->admin_links, '<i class="fa fa-laptop" style="font-size: x-large; vertical-align: bottom; padding: 0 5px; color: #ccddff;" title="The site is run on a local server: '.$IP.'"></i>');
 			$page->head .= '
 			<style>
 				#gp_admin_html #simplepanel .toolbar { 
@@ -26,10 +27,9 @@ class LocalServerAlert {
 			</style>
 			';
 		} else {
-			array_unshift($page->admin_links, '<i class="fa fa-globe" style="font-size: x-large; vertical-align: bottom;" title="The site is run on an online web server" data-toggle="tooltip"></i>');
+			array_unshift($page->admin_links, '<i class="fa fa-globe" style="font-size: x-large; vertical-align: bottom; padding: 0 5px;" title="The site is run on an online web server: '.$IP.'"></i>');
 		}   	
     }
     return $cmd;
   }
-
 }
